2608ff328e042ff18f5af8d435be44d2ef9c0744
jcmcallister <jchar.mc@gmail.com>
2015-04-17 22:57:19 -0400

    todos for client-side AJAX for netcode kickoff

--- js/net.js
+++ js/net.js
@@ -1,1 +1,8 @@
 //network stuff goes here
+
+//TODO: add AJAX functions to server, once server is set up
+
+//TODO: run demo to get Game Rules in JSON from server's Random Game Generator
+
+//TODO: add UI for loading screen
+//TODO: add UI for main menu / nav
