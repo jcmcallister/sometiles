1dce652c08032b8d49f512ac2d44387d774ee7d3
Jeremy McAllister <jeremy@debian.vergen>
2015-05-01 13:16:20 -0400

    client socket.io code

--- js/net.js
+++ js/net.js
@@ -1,16 +1,34 @@
 //network stuff goes here
+function connectSocket(){
+	if(!SomeTiles.hasOwnProperty('rgg')){
+		SomeTiles['rgg'] = io.connect('http://localhost:3000/rgg');
+		logthis("rgg socket is up");
+	}
+
+	if(!SomeTiles.hasOwnProperty('account')){
+		SomeTiles['account'] = io.connect('http://localhost:3000/acct');
+		logthis("account socket is up");
+	}	
+}
+
 
 //TODO: add AJAX functions to server, once server is set up
 function requestGame(cb){
 	
+	connectSocket();
 	logthis("requesting game!");
 	//do AJAX stuff here to req the game: OPTION = use ExpressJS to pipe this over?
-	setTimeout( function(){
-		cb();
-	},2000 );
+	var s = SomeTiles.rgg;
+	s.on('connect',function(){
+		s.emit("request game", function(response){
+			logthis("request game response: " + response);
+		});
+	});
+	
+	cb();
 }
 
-//TODO: run demo to get Game Rules in JSON from server's Random Game Generator
+//TODO: parse Game Rules from JSON from server's Random Game Generator
 function applyGame(){
 
 }
